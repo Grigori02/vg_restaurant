a7a0afc6fdc67bc7d6c4419c15843aa86158ac72
Grigori02 <goqayvazyan98@gmail.com>
2024-06-22 21:56:13 +0300

    commit

--- components/halls/Hallses.jsx
+++ components/halls/Hallses.jsx
@@ -6,7 +6,7 @@ const Halls = () => {
     const arr = [
         {titel:"ՀԱՐՍԱՆՅԱՑ ՍՐԱՀ",qanak:"Քանակ 300-350", image:"https://www.gottable.in/public/frontend/mobile-new/images/rest-detail-img.jpg"},
         {titel:"ԿՆՈՒՆՔԻ և ԾՆՆԴՅԱՆ ՍՐԱՀ",qanak:"Քանակ 100-150", image:"https://img.freepik.com/free-photo/restaurant-hall-with-red-brick-walls-wooden-tables-pipes-ceiling_140725-8504.jpg",},
-        {titel:"ՓՈՔՐ ԱՌԻԹՆԵՐԻ ՍՐԱՀ",qanak:"Քանակ 50-80", image:"https://media.istockphoto.com/id/1162961471/photo/bar-without-guests.jpg?s=612x612&w=0&k=20&c=OPBVyx9axFg7eO5WUbArVkEu-pzDMJLIN5ELH0augpY=",},
+        {titel:" ՓՈՔՐ ԱՌԻԹՆԵՐԻ ՍՐԱՀ",qanak:"Քանակ 50-80", image:"https://media.istockphoto.com/id/1162961471/photo/bar-without-guests.jpg?s=612x612&w=0&k=20&c=OPBVyx9axFg7eO5WUbArVkEu-pzDMJLIN5ELH0augpY=",},
     ]
     return (
         <div className={styles.continer}>
